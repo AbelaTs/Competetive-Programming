187d87ab5c318edcde0f95d4e2377613bae599b7
Abel Tsegaye <abeltsg32@gmail.com>
2019-11-21 23:48:34 +0300

    Reverse Integer upload

---
+++ Day3/ReverseInteger.java
@@ -0,0 +1,33 @@
+class ReverseInteger {
+    public int reverse(int x) {
+        int number = x;
+        if(x<0){
+            number = Math.abs(x);
+        }
+        String num = Integer.toString(number);
+        int reversed_num = 0;
+        char[] chars = new char[num.length()];
+       
+        int count = 0;
+        
+        for(int i=num.length()-1; i>=0; i--){
+               chars[count] = num.charAt(i); 
+               count++;
+            
+        }
+        String rev_num = new String(chars);
+        try{
+        if(x < 0){
+            reversed_num = Integer.parseInt(rev_num);
+            reversed_num = 0-reversed_num;
+        } else{
+            reversed_num = Integer.parseInt(rev_num);
+            }
+    
+        }catch(NumberFormatException ex){
+            
+        }
+        return reversed_num;
+        
+    }
+}
