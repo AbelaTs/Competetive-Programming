fede6f337758e919bf6aec80e74a10249433413f
Abel Tsegaye <abeltsg32@gmail.com>
2019-12-02 00:53:27 +0300

    H-index problem solved

---
+++ Day 6/HIndex.java
@@ -0,0 +1,27 @@
+class HIndex {
+    public int hIndex(int[] citations) {
+        int count = 0;
+        int at_least = 0;
+       
+            for(int i = 0; i< citations.length; i++){
+                count = 0;
+                for (int j = 0; j < citations.length; j++){
+                    if(citations[j] - citations[i] >= 0){
+                        count++;
+                    }
+                    if(citations[i] >= count){
+                        if(count > at_least){
+                            at_least = count;
+                        }
+                    }else{
+                         if(citations[i] > at_least){
+                            at_least = citations[i];
+                        }
+                    }
+                 
+                }
+            }
+            return at_least;
+
+    }
+}
