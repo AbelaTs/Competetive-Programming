2404d40a3ef1ebfa974e98b43394404505393aaa
Abel Tsegaye <abeltsg32@gmail.com>
2019-12-03 19:27:07 +0300

    Modification

--- Day 11/DeleteNode.java
+++ Day 11/DeleteNode.java
@@ -1,12 +1,7 @@
 public class DeleteNode{
     public void deleteNode(ListNode node) {
-        ListNode node_to_delete;
-        int value;
-        node_to_delete = node.next;
-        value = node_to_delete.val;
-        node_to_delete = node_to_delete.next;
-        node.next = node_to_delete;
-        node.val = value;
+        node.val = node.next.val;
+        node.next = node.next.next;
     }
     public static void main(String args[]){
 
