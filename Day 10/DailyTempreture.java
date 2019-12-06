0e9d8df5738f119f01db96f521c223d8ce59909b
Abel Tsegaye <abeltsg32@gmail.com>
2019-12-06 15:19:10 +0300

    New Solutions added

---
+++ Day 10/DailyTempreture.java
@@ -0,0 +1,22 @@
+class DailyTempreture {
+    public int[] dailyTemperatures(int[] T) {
+        int[] result = new int[T.length];
+        int counter;
+        for(int x =0; x<T.length; x++){
+            counter = 0;
+            for(int y = x+1; y<T.length; y++){
+                if(T[y] > T[x]){
+                    counter++;
+                    result[x] = counter;
+                    break;
+                }
+                counter++;
+            }
+            if(Integer.toString(result[x]).length < 1){
+                result[x] = 0;
+            }
+
+        }
+        return result;
+    }
+}
---
+++ Day 11/MergeSortedList.java
@@ -0,0 +1,47 @@
+
+ //Definition for singly-linked list.
+  public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+ }
+ 
+class MergeSortedList{
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode start = new ListNode(0);
+        start.next = head;
+        if(l1 == null && l2 !=null){
+            return l2;
+        }else if(l1 != null && l2 != null){
+                while(l1 != null && l2 !=null){
+                    if(l1.val <= l2.val){
+                        head.next = l1;
+                        head = head.next;
+                        l1 = l1.next;
+                    }else{
+                        head.next = l2;
+                        head = head.next;
+                        l2 = l2.next;
+
+                    }
+
+
+                }
+                
+            
+                if(l1 == null && l2 != null){
+                    head.next = l2;
+                }else if(l2 == null & l1 != null){
+                    head.next = l1;
+                }
+
+
+            
+        }else if(l1 != null && l2 ==null){
+            return l1;
+        }
+     
+        return start.next.next; 
+    }
+}
