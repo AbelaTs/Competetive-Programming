f287053b2e484e204a040592409dffc33ba6c013
Abel Tsegaye <abeltsg32@gmail.com>
2019-12-11 00:51:47 +0300

    New solutions added

---
+++ Day 14/SearchBinaryTree.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class SearchBinaryTree {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null){
+            return null;
+        }else{
+                if(root.val == val){
+                    return root;
+                }
+                root.left = searchBST(root.left,val);
+                root.right = searchBST(root.right,val);
+                
+            
+            if(root.left != null){
+                return root.left;
+            }
+            return root.right;
+        }
+    }
+}
