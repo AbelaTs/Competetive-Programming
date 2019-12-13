705e8485176d74fca91bbb93ee9a7116c8cf5ae3
Abela <abeltsg32@gmail.com>
2019-12-13 13:38:46 -0900

    Modification

--- Day 14/LowestCommonAncestor.java
+++ Day 14/LowestCommonAncestor.java
@@ -8,73 +8,33 @@ class TreeNode {
  }
  
 public class LowestCommonAncestor {
-    TreeNode left_root;
-    TreeNode right_root;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null){
-            return null;
+             
+    if(root == null){
+        return null;
+    }
+      TreeNode left_root = null;
+      TreeNode right_root = null;
+        if(root.val == p.val || root.val == q.val){
+                return root;  
         }else{
-            if(root.val == p.val || root.val == q.val){
+            if(root.left != null){
                 left_root = lowestCommonAncestor(root.left,p,q);
-                right_root = lowestCommonAncestor(root.right,p,q);
-                if(left_root != null || right_root != null){
-                    return root;
-                }else{
-                    return root; 
-                }
-               
-            }else if(root.left != null && root.right != null){//If node has both right and left
-                
-                if(root.left.val == p.val && root.right.val == q.val || root.left.val == q.val && root.right.val == p.val){
-                    return root;
-                    
-                }else{
-                        
-                    left_root = lowestCommonAncestor(root.left,p,q);
-                    right_root = lowestCommonAncestor(root.right,p,q);
-                }   
-              
-                  
-            }else if(root.left != null && root.right == null){//If node has left but no right
-                
-                if(root.val == p.val || root.val == q.val){
-                    left_root = lowestCommonAncestor(root.left,p,q);
-                    if(left_root != null){
-                        return left_root;
-                    }else{
-                        return null;
-                    }
-                }else{
-                        
-                    left_root = lowestCommonAncestor(root.left,p,q);
-                    right_root = lowestCommonAncestor(root.right,p,q);
-                } 
-                
-            }else if(root.right != null && root.left == null){//If node has right but no left
-                
-               if(root.val == p.val || root.val == q.val){
-                    right_root = lowestCommonAncestor(root.right,p,q);
-                    if(right_root != null){
-                        return right_root;
-                    }else{
-                        return null;
-                    }
-                }else{
-                        
-                    left_root = lowestCommonAncestor(root.left,p,q);
-                    right_root = lowestCommonAncestor(root.right,p,q);
-                } 
             }
-              //Returning the result
-                if(left_root != null && right_root != null){
-                    return root;
-                }else if(left_root != null){
-                     return left_root;
-                }else if(right_root != null){
-                     return right_root;
-                }else{
-                  return null;
-              } 
+            if(root.right != null){
+                right_root = lowestCommonAncestor(root.right,p,q);
+            }
+            
+             if(left_root != null && right_root != null){
+                return root;
+            }else if(left_root != null && right_root == null){
+                return left_root;
+            }else if(right_root != null && left_root == null){
+                return right_root;
+            }else{
+                return null;
+            }
         }
+        
     }
 }
