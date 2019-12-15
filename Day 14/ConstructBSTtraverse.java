6a733dd5ea93a71d0e32accca4bf87567691bd86
Abel Tsegaye <abeltsg32@gmail.com>
2019-12-15 09:08:16 +0300

    New solution added

---
+++ Day 14/ConstructBSTtraverse.java
@@ -0,0 +1,77 @@
+
+// Definition for a binary tree node.
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+
+class ConstructBSTtraverse {
+ 
+    public static TreeNode bstFromPreorder(int[] preorder) {
+        
+        if(preorder.length == 3){
+           TreeNode node = new TreeNode(preorder[0]);
+           if(preorder[1] < node.val){
+               node.left = new TreeNode(preorder[1]);
+           }else if(preorder[1] > node.val){
+               TreeNode node1  = new TreeNode(preorder[1]);
+               node.right = node1;
+               if(preorder[2] < node1.val){
+                   
+                   node1.left = new TreeNode(preorder[2]);
+               }else if(preorder[2] > node1.val){
+                   node1.right = new TreeNode(preorder[2]);
+               }
+               
+           }
+            //if left node is already taken
+            if(node.left != null){
+                if(preorder[2] > node.val){
+                    node.right = new TreeNode(preorder[2]);  
+                }else if(preorder[2] < node.val){
+                    if(preorder[2] < node.left.val){
+                        node.left.left = new TreeNode(preorder[2]);
+                    }else if(preorder[2] > node.left.val){
+                        node.left.right = new TreeNode(preorder[2]);
+                    }
+                }
+            }
+           
+            
+            
+            return node;
+            
+        }else if(preorder.length == 2){
+            TreeNode node = new TreeNode(preorder[0]);
+            if(preorder[1] < node.val){
+               node.left = new TreeNode(preorder[1]);
+            }else if(preorder[1] > node.val){
+               node.right = new TreeNode(preorder[1]);
+            }
+            return node;
+        }else if(preorder.length == 1){
+            TreeNode node = new TreeNode(preorder[0]);
+            return node;
+        }else if(preorder.length == 0){
+            return null;
+        }
+        TreeNode node = new TreeNode(preorder[0]);
+        int counter = 0;
+        for(int i = 1; i < preorder.length; i++){
+            if(preorder[i] <= node.val){
+                counter++;
+            }else{
+                break;
+            }
+        }
+        int[] l1 = Arrays.copyOfRange(preorder,1,counter+1);
+        node.left = bstFromPreorder(l1);
+        int[] l2 = Arrays.copyOfRange(preorder,counter+1,preorder.length);
+        node.right = bstFromPreorder(l2);
+        return node;
+    }
+
+    
+}
