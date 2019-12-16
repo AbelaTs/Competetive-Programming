23f522833f36bf4e6555d724ba58c5a2b9bd5c49
Abel Tsegaye <abeltsg32@gmail.com>
2019-12-16 17:44:38 +0300

    Modification

--- Day 16/MinimumDistanceBST.java
+++ Day 16/MinimumDistanceBST.java
@@ -9,28 +9,90 @@
 class MinimumDistanceBST {
     static int min;
     public static int minDiffInBST(TreeNode root) {
-       if(root != null){
-           ArrayList<Integer> values = new ArrayList<Integer>();
-            findMin(root,values);
-            int min = values.get(0);
-            //Check the distance between each node
-            for(int i =0; i < values.size(); i++){
-                for(int j = 0; j < values.size(); j++){
-                    if(values.get(i) != values.get(j)){
-                        if(Math.abs(values.get(i) - values.get(j)) < min){
-                            min = Math.abs(values.get(i) - values.get(j));
-                        }
-                    }
-                }
-            }
-
-
-            return min;
-       }else{
-           return 0;
-       }
+        if(root != null){
+            ArrayList<Integer> values = new ArrayList<Integer>();
+             findMin(root,values);
+             
+             int[] result = new int[values.size()];
+             for(int i = 0; i < values.size(); i++){
+                 result[i] = (int) values.get(i);
+             }
+             result = mergeSort(result);
+             int min = result[1] - result[0];
+ 
+             for(int i=0; i<result.length-1; i++){
+                 if(result[i+1] - result[i] < min){
+                     min = result[i+1] - result[i];
+                 }
+             }
+ 
+ 
+             return min;
+        }else{
+            return 0;
+        }
+         
         
     }
+
+    //Merge Sort
+    public static int[] merge(int[] left_list,int[] right_list){
+        int merge_list_length = left_list.length + right_list.length;
+        int[] merge_list = new int[merge_list_length];
+        int left_pointer = 0;
+        int right_pointer = 0;
+        
+        int merged_list_pointer = 0;
+
+        while(merged_list_pointer < merge_list_length){
+           if(left_pointer < left_list.length && right_pointer < right_list.length){
+                if(left_list[left_pointer] <= right_list[right_pointer]){
+                    merge_list[merged_list_pointer] = left_list[left_pointer];
+                    left_pointer++;
+                }else{
+                merge_list[merged_list_pointer] = right_list[right_pointer];
+                right_pointer++;
+                }
+                merged_list_pointer++;
+
+
+           }else{
+               if(left_pointer >= left_list.length){
+                   for(int i = right_pointer; i< right_list.length; i++){
+                       merge_list[merged_list_pointer] = right_list[right_pointer];
+                       right_pointer++;
+                       merged_list_pointer++;
+                   }
+               }else if(right_pointer >= right_list.length){
+                    for(int i = left_pointer; i< left_list.length; i++){
+                        merge_list[merged_list_pointer] = left_list[left_pointer];
+                        left_pointer++;
+                        merged_list_pointer++;
+                }
+               }
+           }
+           
+        }
+
+        return merge_list;
+    }
+
+    // sorts a gien list in an increasing order
+    public static int[] mergeSort(int[] list){
+        int[] left_list = Arrays.copyOfRange(list,0,list.length/2);
+        int[] right_list = Arrays.copyOfRange(list,list.length/2,list.length);
+
+        if(list.length == 1){
+            return list;
+        }else{
+            left_list = mergeSort(left_list);
+            right_list = mergeSort(right_list);
+        }
+        return merge(left_list,right_list);
+
+
+    }
+
     //Recurseive function to get the difference between each different node
     public static TreeNode findMin(TreeNode root,ArrayList<Integer> list){
         if(root != null){
