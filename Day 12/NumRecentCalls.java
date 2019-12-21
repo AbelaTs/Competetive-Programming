8b0b44b6537f5e95fd663daa4de8be4c2c2ab5c6
Abel Tsegaye <abeltsg32@gmail.com>
2019-12-21 15:08:09 +0300

    New solutions added

---
+++ Day 12/NumRecentCalls.java
@@ -0,0 +1,23 @@
+class   NumRecentCalls {
+    static ArrayList<Integer> time_units;
+    public NumRecentCalls() {
+        time_units = new ArrayList<Integer>();
+    }
+    
+    public static int ping(int t) {
+        int counter = 0;
+        time_units.add(t);
+        for(int i = 0; i < time_units.size(); i++){
+            if(time_units.get(i) >= t - 3000 && time_units.get(i) <= t){
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
