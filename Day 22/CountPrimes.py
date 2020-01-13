f65bb8eab14887a7460a68f2c17db98febe7c878
Abela <abeltsg32@gmail.com>
2020-01-13 19:19:58 +0300

    New solutions added

---
+++ Day 22/CountPrimes.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def countPrimes(self, n):
+        boundary = int(Math.floor(Math.sqrt(n)))
+        counter = 0
+        isPrime = []
+        for num in range(n):
+            isPrime.append(True)
+        isPrime[0] = False
+        isPrime[1] = False
+        for num in range(2,boundary):
+            for i in range(len(isPrime)):
+                isprime[num*i] = False
+        for count in isPrime:
+            if(count):
+                counter += 1
+        return counter
---
+++ Day 22/Power.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def myPow(self, x, n):
+        
+        if n >=0:
+            if n == 0:
+                return 1
+            elif n % 2 == 0:
+                y = self.myPow(x, n/2)
+                return y**2
+            else:
+                return x * self.myPow(x, n-1)
+        else:
+            if n == 0:
+                return 1
+            elif n % 2 == 0:
+                y = self.myPow(x, n/2)
+                return y**2
+            else:
+                return (1/x) * self.myPow(x, n+1)
