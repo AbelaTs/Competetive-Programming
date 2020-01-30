c3f8067472cfb981414c5b0a62a507b8b8f8b78e
Abela <abeltsg32@gmail.com>
2020-01-30 16:28:47 +0300

    New solutions added

---
+++ Code Camp Contests/Contest 3/NumberOfEquivalentDominos.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+class NumberOfEquivalentDominos {
+    public int numEquivDominoPairs(int[][] dominoes) {
+        HashMap<String,Integer> mapper = new HashMap<String,Integer>();
+        int counter = 0;
+        for(int i = 0; i < dominoes.length; i++){
+            if(mapper.containsKey(dominoes[i][0]+""+dominoes[i][1])){
+                counter+= mapper.get(dominoes[i][0]+""+dominoes[i][1]);
+                mapper.put(dominoes[i][0]+""+dominoes[i][1],mapper.get(dominoes[i][0]+""+dominoes[i][1])+1);  
+                
+            }
+            if(dominoes[i][0] != dominoes[i][1]){
+                if(mapper.containsKey(dominoes[i][1]+""+dominoes[i][0])){
+                    counter+=mapper.get(dominoes[i][1]+""+dominoes[i][0]);
+
+                }
+            }
+            if(!mapper.containsKey(dominoes[i][0]+""+dominoes[i][1])){
+                mapper.put(dominoes[i][0]+""+dominoes[i][1],1);
+            }
+        }
+        return counter;
+    }
+    
+    }
