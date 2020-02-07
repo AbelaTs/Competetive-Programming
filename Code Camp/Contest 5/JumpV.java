b98472b2a5f304839bdca94333c9cd0605da29d2
Abela <abeltsg32@gmail.com>
2020-02-07 09:36:23 +0300

    New solutions added

---
+++ Code Camp/Contest 5/JumpV.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+class JumpV {
+    public int maxJumps(int[] arr, int d) {
+     HashMap<Integer,ArrayList<Integer>> mapper = new HashMap<Integer,ArrayList<Integer>>();
+     for(int i = 0; i < arr.length; i++){
+         mapper.put(i,new ArrayList<Integer>());
+         //right
+         for(int j = 1; j <= d; j++){
+             if(i+j < arr.length){
+                 if(arr[i] > arr[i+j]){
+                    mapper.get(i).add(i+j);
+
+                 }else{
+                     break;
+                 }
+             }
+         }
+         //left
+         
+         for(int j = 1; j <= d; j++){
+             if(i-j >= 0){
+                 if(arr[i] > arr[i-j]){
+                     mapper.get(i).add(i-j);   
+                 }else{
+                     break;
+                 }
+             }
+         }
+         
+        }
+         
+         int[] jumpVal = new int[arr.length];
+         boolean[] seen = new boolean[arr.length];
+         for(int i = 0; i < arr.length; i++){
+             getMaxJump(arr,jumpVal,seen,i,mapper);
+         }
+         int max_jump = 1;
+         for(int i = 0; i < jumpVal.length; i++){
+             if(jumpVal[i] > max_jump){
+                 max_jump = jumpVal[i];
+             }
+         }
+         return max_jump;
+        
+    }
+    public int getMaxJump(int[]arr,int[] jump_count,boolean[] seen,int index,HashMap<Integer,ArrayList<Integer>> mapper){
+        if(seen[index]){
+            return jump_count[index];
+        }
+        int max_jump = 0;
+        for(Integer neighbour : mapper.get(index)){
+            
+            int count = getMaxJump(arr,jump_count,seen,neighbour,mapper);
+            if(count > max_jump){
+                max_jump = count;
+            }
+        }
+        jump_count[index] = max_jump+1;
+        seen[index] = true;
+        return jump_count[index];
+    }
+}
