41b2dc629395cc271c766903457e6ce5aecf18ba
Abela <abeltsg32@gmail.com>
2020-02-14 01:56:18 +0300

    New solution added

---
+++ Code Camp/Dynamic Programming/Keys2.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+class Keys2 {
+    public int minSteps(int n) {
+       ArrayList<Integer> A_present = new ArrayList<Integer>(); 
+       A_present.add(1);
+       int step_counter = 0;
+       int copied = 0;
+       int written = 1;
+       while(written < n){
+           //if the word to be written is divisible by already written then we can copy and paste
+           if(n % written == 0){
+               copied = written;//copy
+               step_counter++;
+           }
+           step_counter++;//pasting the copied
+           written += copied;
+           
+           
+       }
+       return step_counter;
+        
+    }
+}
