55ca079fe0a3da0874565f4f90caefafe52dbbf4
Abela <abeltsg32@gmail.com>
2020-02-19 02:26:04 +0300

    New solutions added

---
+++ Day 19/MergeKsortedList.java
@@ -0,0 +1,34 @@
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
+//Definition for singly-linked list. 
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+    }
+ 
+class MergeKsortedList {
+    public ListNode mergeKLists(ListNode[] lists) {
+      PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+      for(int i = 0; i < lists.length; i++){
+          ListNode temp = lists[i];
+          while(temp != null){
+              minHeap.add(temp.val);
+              temp = temp.next;
+          }
+      }
+      ListNode res = new ListNode(0);
+      ListNode temp = res;
+        
+      Iterator iterator = minHeap.iterator();
+      while(iterator.hasNext()){
+          ListNode node = new ListNode(minHeap.poll());
+          temp.next = node;
+          temp = temp.next;
+      }
+          
+      
+        return res.next;
+    }
+}
